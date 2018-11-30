26221fe1b79932697077f17244db3ae3ad2f6c13
Loc Tran <loc@route42.nl>
2018-12-01 00:27:45 +0100

    formatting

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,13 @@ MAINTAINER Loc Tran <loc@route42.nl>
 
 # Update
 RUN apt-get update && \
-	apt-get install -y golang && \
+    apt-get install -y golang && \
     apt-get dist-upgrade -y && \
     apt-get install -y ca-certificates && \
     apt-get install -y gcc-multilib && \
     apt-get install -y gcc-mingw-w64 && \
-	apt-get install -y git && \
-	apt-get install -y libpcap-dev
+    apt-get install -y git && \
+    apt-get install -y libpcap-dev
 
 # Config
 VOLUME ["/usr/local/src"]
--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 # Run
-docker run -it -v `pwd`:/usr/local/app teecp sh -c 'go get ./... && GOOS=windows GOARCH=amd64 CGO_ENABLED=1  CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc CGO_CFLAGS="-I/usr/local/app/include" CGO_LDFLAGS="-L/usr/local/app/lib" go build -o teecp.exe .'
+docker run \
+    -it \
+    -v `pwd`:/usr/local/app teecp \
+    sh -c 'go get ./... && GOOS=windows GOARCH=amd64 CGO_ENABLED=1  CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc CGO_CFLAGS="-I/usr/local/app/include" CGO_LDFLAGS="-L/usr/local/app/lib" go build -o teecp.exe .'
