7ec2b6bd7199c31d54964b487c06dfe29ba33863
Loc Tran <loc@route42.nl>
2018-12-01 00:24:45 +0100

    docker cross compile

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM ubuntu:18.04
+MAINTAINER Loc Tran <loc@route42.nl>
+
+# Update
+RUN apt-get update && \
+	apt-get install -y golang && \
+    apt-get dist-upgrade -y && \
+    apt-get install -y ca-certificates && \
+    apt-get install -y gcc-multilib && \
+    apt-get install -y gcc-mingw-w64 && \
+	apt-get install -y git && \
+	apt-get install -y libpcap-dev
+
+# Config
+VOLUME ["/usr/local/src"]
+
+RUN mkdir -p /usr/local/app
+ENV GOPATH=/usr/local/app/gopath/
+ENV GOBIN=/usr/local/app/src/
+
+WORKDIR /usr/local/app/src/
---
+++ src/conf.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/google/gopacket/pcap"
+	"log"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+type Opts struct {
+	Device        string
+	OutputTcp     string
+	MaxPacketSize int32
+	QueueSize     int
+	NumForwarders int
+	StatsPrinter  bool
+
+	layers map[int]bool
+}
+
+func NewOpts() *Opts {
+	return &Opts{
+		MaxPacketSize: 65536,
+		QueueSize:     1000,
+		NumForwarders: runtime.NumCPU(),
+	}
+}
+
+func (opts *Opts) Print() {
+	j, _ := json.Marshal(opts)
+	log.Printf("config %s", string(j))
+}
+
+func (opts *Opts) Validate() {
+	// @todo check that it's valid configuration
+}
+
+func (opts *Opts) ParseLayers(layerStr string) {
+	opts.layers = make(map[int]bool)
+	for _, str := range strings.Split(layerStr, ",") {
+		i, _ := strconv.ParseInt(strings.TrimSpace(str), 10, 64)
+		if i > 0 {
+			opts.layers[int(i)] = true
+		}
+	}
+	if len(opts.layers) < 1 {
+		// no filter
+		opts.layers = nil
+	}
+}
+
+func (opts *Opts) AutoDiscover() {
+	if len(strings.TrimSpace(opts.Device)) > 0 {
+		// already configured
+		return
+	}
+
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print device information
+	if verbose {
+		log.Println("Devices found:")
+		for _, device := range devices {
+			log.Printf("Name: %s", device.Name)
+			log.Printf("Description: %s", device.Description)
+			for _, address := range device.Addresses {
+				log.Printf("- IP address: %s", address.IP.String())
+				log.Printf("- Subnet mask: %s", address.Netmask.String())
+			}
+		}
+	}
+
+	opts.Device = devices[0].Name
+}
