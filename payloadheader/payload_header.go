62f542ba5dbdd0ebc2959858e460e8b097be0130
Robin <robin+github@us2.nl>
2018-12-05 09:14:34 +0100

    move

--- payloadheader/payload_header.go
+++ payloadheader/payload_header.go
@@ -1,4 +1,4 @@
-package main
+package payloadheader
 
 import (
 	"bytes"
--- payloadheader/payload_header_test.go
+++ payloadheader/payload_header_test.go
@@ -1,19 +1,20 @@
-package main
+package payloadheader_test
 
 import (
+	"../payloadheader"
 	"log"
 	"net"
 	"testing"
 )
 
 func TestNewPayloadHeader(t *testing.T) {
-	header := NewPayloadHeader(len([]byte("test")), net.ParseIP("127.0.0.1"), 1234, net.ParseIP("10.0.0.4"), 9999)
+	header := payloadheader.NewPayloadHeader(len([]byte("test")), net.ParseIP("127.0.0.1"), 1234, net.ParseIP("10.0.0.4"), 9999)
 	log.Printf("%+v", header)
 	b := header.Bytes()
 	log.Printf("%d %x", len(b), b)
 
 	{
-		readHeader := PayloadHeaderFromBytes(b)
+		readHeader := payloadheader.PayloadHeaderFromBytes(b)
 		log.Printf("%+v", readHeader)
 		src := readHeader.SrcIP()
 		if src.String() != "127.0.0.1" {
