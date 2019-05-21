abce8fba4f7bc102f5e16e0b33bc9acd10ea3ab3
Robin <robin+github@us2.nl>
2019-05-21 16:08:35 +0200

    path fix

--- docker_run.sh
+++ docker_run.sh
@@ -3,4 +3,4 @@
 docker run \
     -it \
     -v `pwd`:/usr/local/app teecp \
-    sh -c 'go get "github.com/google/gopacket" && go get "github.com/google/gopacket/pcap" && go get "github.com/pkg/errors" && /usr/local/app/test.sh && GOOS=windows GOARCH=amd64 CGO_ENABLED=1  CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc CGO_CFLAGS="-I/usr/local/app/include" CGO_LDFLAGS="-L/usr/local/app/lib" go build -o teecp.exe .'
+    sh -c 'export PATH=$PATH:/usr/lib/go-1.12/bin && go get "github.com/google/gopacket" && go get "github.com/google/gopacket/pcap" && go get "github.com/pkg/errors" && /usr/local/app/test.sh && GOOS=windows GOARCH=amd64 CGO_ENABLED=1  CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc CGO_CFLAGS="-I/usr/local/app/include" CGO_LDFLAGS="-L/usr/local/app/lib" go build -o teecp.exe .'
